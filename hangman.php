b2e341b361ee988b092353cc2cf1e54666d50afa
evelynokner <oknerevelyn@gmail.com>
2025-04-11 12:54:29 -0400

    Update hangman.php
    complete game

--- hangman.php
+++ hangman.php
@@ -44,17 +44,21 @@ $hangman_img = ["
       ==="];
 
 $words = ["SERVER", "GEORGIAN", "COMPUTER", "PROJECT"];
+// word randomly picked from $words array
 $random_word;
+// underscores display for the number of letters there are in the random word
 $template;
-$user_guess; 
+// when user gets to 6 failed guesses, the game ends
 $fails = 0;
+$user_guess;
 $guessed_letters = array();
 
-if(readline("Would you like to play Hangman?") == "Y"){
+if(strtoupper(readline("Would you like to play Hangman?\nY for yes, N for no: ")) == "Y"){
     play_round();
+    if(readline("Play again?\n") == "Y"){
+        play_round();
+    }
 }
-
-
 
 function play_round(){
     global $words, $random_word, $hangman_img;
@@ -65,49 +69,37 @@ function play_round(){
     // display first hangman image initially
     echo "$hangman_img[0]\n";
     
-    while(true){
-        //echo "Guess a letter: \n";
-            make_guess();
-            display_hangman();
-            if(gameover()) break;
-            //return true;
-        //}
-        //else break;
-    } 
-    echo "Thanks for playing!";
-}
-function make_guess(){
-    // while loop 
-    
-    
-    
-    global $hangman_img, $random_word, $fails;
-    
-    
-    ##### TESTING PURPOSES #####
-    
-    
+    // display this when game starts so user knows the number of letters in the word
     // for each letter in the randomly selected word, display "_"
-    ###### change to $words to $random_word ######
     for($i = 0; $i <= strlen($random_word)-1; $i++){
         echo "_ ";
-    } echo "\n"; // line break after displaying all underscores
+    } echo "\n\n";
+    
+    while(true){
+        make_guess();
+        //display_hangman();
+        if(gameover()) break;
+    } 
+    echo "Thanks for playing!\n";
+}
+function make_guess(){
+    global $hangman_img, $random_word, $fails;
     
     echo "Please guess a letter: ";
-    $user_guess = readline();
-    echo "User guessed: $user_guess \n";
+    // allow user to type in lowercase letters and they will convert to uppercase
+    $user_guess = strtoupper(readline());
+    
     // if user guesses correctly, change letter display
     if (apply_guess($user_guess)){
-         display_guessed_letter();
+        echo $user_guess, " is correct!\n\n";
+        display_guessed_letter();
     }
-    // otherwise, display the next hangman image
+    // if user guesses wrong, display the next hangman image
     else {
+        echo "Sorry, ", $user_guess, " is not in the word.\n";
         $fails++;
         display_hangman();
     }
-    //display_guessed_letter();
-    // if guessed letter is in the word, tell user their guess is correct and prompt them to continue guessing
-    // for each letter in $random_word, if i = $user_guess, replace the underscore with that letter
 }
 
 function display_hangman(){
@@ -115,13 +107,13 @@ function display_hangman(){
     // if $hangman_img[6] is displayed, game over
     global $hangman_img, $fails;
     // display hangman image at position of number of fails
-    echo $hangman_img[$fails];
+    echo $hangman_img[$fails], "\n";
 }
 
 // displays updated state of game after user guesses a correct letter
 function display_guessed_letter(){
     global $guessed_letters, $random_word, $template;
-    $template = str_repeat(".", strlen($random_word));
+    $template = str_repeat("_", strlen($random_word));
     for($i=0; $i<=strlen($template)-1; $i++){
        if (in_array($random_word[$i], $guessed_letters)){
        	    //echo $word[$i], "\n";
@@ -131,11 +123,10 @@ function display_guessed_letter(){
     echo $template;
 }
 
+// this function displays the user's correct guesses so they know the position of their guessed letter in the word (e.g. "PR_J__T")
 function apply_guess($user_guess){
-    //global $user_guess;
-    global $guessed_letters, $random_word;
+    global $random_word, $guessed_letters;
     $correct = false;
-    $update = "";
     for($i = 0; $i <= strlen($random_word)-1; $i++){
         $letter = $random_word[$i];
         if($letter == $user_guess){
@@ -148,24 +139,19 @@ function apply_guess($user_guess){
     return $correct;
 }
 
-function incorrect_guess(){
-    // incorrect guesses variable (?)
-    // if incorrect guesses == 6, game over
-    // aka if $hangman_img[6] is displayed, game over
-}
-
+// determines if game is over, returns boolean
 function gameover(){
     /* game over if:
         - user has 6 wrong guesses ($hangman_img[6] was printed)
         - user has guessed every letter correctly 
-         */
+    */
     global $fails, $template, $random_word;
-    // player loses if # of fails reaches 6
+    
+    // player loses if number of fails reaches 6
     if($fails == 6) return true;
     // player wins if their guesses match the random word
     if($template == $random_word) return true;
     return false;
 }
-
 
 ?>
